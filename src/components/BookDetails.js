d0d0aeac7adc0c5f4a8e7bc47fd6f2bbb9021d11
serhiisergio <dmitriievserguei@gmail.com>
2022-07-06 13:51:44 +0400

    deploy my react app to my github pages

---
+++ src/components/BookDetails.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+function BookDetails({ book }) {
+  const { dispatch } = useContext(BookContext);
+  const handleRemove = () => {
+    dispatch({ type: "REMOVE_BOOK", id: book.id });
+  };
+  return (
+    <li onClick={handleRemove}>
+      <div className="title">{book.title}</div>
+      <div className="author">{book.author}</div>
+    </li>
+  );
+}
+
+export default BookDetails;
